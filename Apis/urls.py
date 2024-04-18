00c864ca5b8ef460b7853971f8df91337fabd8cd
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2024-04-18 09:44:01 +0530

    Done

--- Apis/urls.py
+++ Apis/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register', RegisterAPIView.as_view(), name='register'),
     path('login', LoginAPIView.as_view(), name='login'),
     # /session
-    path('authTn/dynaQuery', JobResumeDynamicQuery.as_view(), name='dynaQuery'),
+    path('auth/dynaQuery', JobResumeDynamicQuery.as_view(), name='dynaQuery'),
 ]
